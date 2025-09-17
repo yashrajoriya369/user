b47e7f50e78e71b0910639034daf92c7f1c22987
Yash Rajoriya <yashrajoriya369@gmail.com>
2025-09-17 13:10:14 +0530

    final commit

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
-// export const base_url = "http://localhost:8080/api/";
-export const base_url = "https://swar-backend.vercel.app/";
+export const base_url = "http://localhost:8080/api/";
+// export const base_url = "http://swar-backend.vercel.app/";
